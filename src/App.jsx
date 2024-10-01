1cdff9cf729fc4ba1efa2a211399b49b24edb331
V-TUSHAR07 <tusharpatwadi@gmail.com>
2024-10-01 13:45:26 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Login from "./components/auth/Login";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div>
-      <Login />
+      {/* <Login /> */}
+      <Home />
     </div>
   );
 }
